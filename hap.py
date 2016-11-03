6577a2fcaa82a742a6839592454a8658a81cb4a5
Richard Wang <richardwang@Richards-MacBook-Pro-2.local>
2016-11-03 14:35:47 -0400

    initial setup on hap

---
+++ hap.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+
+app.secret_key = '\x90\xfb\x0f6\x1dY\xa5i\x93+m\x83\xd8\xd9\xad\x91}\xef\x95]_\xe2i\xde\xcc\xb7\x03c\x83\xf3\xd1J'
+
+app = Flask(__name__)
+
+@app.route("/")
+
+@app.route("/register")
+
+@app.route("/settings")
+
+@app.route("/add")
+
+if __name__ == "__main__":
+    app.debug = "TRUE"
+    app.run()
