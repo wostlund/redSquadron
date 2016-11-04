dab695470e3bc6d93b746c6120044d836263fa9e
Richard Wang <richardwang@Richards-MacBook-Pro-2.local>
2016-11-04 14:48:25 -0400

    init stuff

--- hap.py
+++ hap.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 
-app.secret_key = '\x90\xfb\x0f6\x1dY\xa5i\x93+m\x83\xd8\xd9\xad\x91}\xef\x95]_\xe2i\xde\xcc\xb7\x03c\x83\xf3\xd1J'
+
 
 app = Flask(__name__)
+
+app.secret_key = '\x90\xfb\x0f6\x1dY\xa5i\x93+m\x83\xd8\xd9\xad\x91}\xef\x95]_\xe2i\xde\xcc\xb7\x03c\x83\xf3\xd1J'
 
 @app.route("/")
 def login():
@@ -10,7 +12,7 @@ def login():
         return redirect(url_for('welcome'))
     return render_template('login.html', error = False)
 
-@app.route("/register")
+#@app.route("/register")
 
 @app.route("/welcome/")
 def welcome():
@@ -25,10 +27,10 @@ def logout():
         session.pop('username', None)
     return redirect(url_for('login'))
 
-@app.route("/settings")
+#@app.route("/settings")
 
-@app.route("/add")
+#@app.route("/add")
 
 if __name__ == "__main__":
-    app.debug = "TRUE"
+    app.debug = True
     app.run()
