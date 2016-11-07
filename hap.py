9e9e17d58c1f67655ab5a74befd3d75c82a5a32e
Richard Wang <richardwang@Richards-MacBook-Pro-2.local>
2016-11-07 02:59:50 -0500

    fixed silly

--- hap.py
+++ hap.py
@@ -26,7 +26,7 @@ def authenticate():
             if result == "Bad Login":
                 return render_template("login.html",message="Username or Password invalid. Try again.")
             else:
-                session['user'] = request.form['user']
+                session['username'] = request.form['user']
                 return redirect(url_for('welcome'))
         elif button_val == "reg":
             print ('before check reg')
