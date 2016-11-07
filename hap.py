aee9f0eb93e9ab37d2bfc20fe04dfa444f866ea4
P0rkins <lvargas@stuy.edu>
2016-11-06 21:43:04 -0500

    check it out

--- hap.py
+++ hap.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.secret_key = '\x90\xfb\x0f6\x1dY\xa5i\x93+m\x83\xd8\xd9\xad\x91}\xef\x95]_\xe2i\xde\xcc\xb7\x03c\x83\xf3\xd1J'
 
-db_access.create_db()
+dbaccess.create_db()
 db = sqlite3.connect(data/data.db)
 
 @app.route("/")
@@ -15,19 +15,23 @@ def login():
         return redirect(url_for('welcome'))
     return render_template('login.html', error = False)
 
-@app.route("/register/",methods=["POST"])
+@app.route("/authenticate",methods=["POST"])
 def registration():
     if request.method=="POST":
         u=request.form["username"]
         p=hashIt(request.form["pass"])
-        result = check_reg(u, p)
-        if result  == "Username taken":
-            return render_template("login.html",message="Username taken. Be more original")
-        elif result == "Invalid username":
-            return render_template("login.html",message="Invalid username. Please change it.")
-        else:
-            add_account(u, p)
-            return render_template("login.html",message="Success! Your account has been created.")
+        button_val = request.form["val"]
+        if button_val == "log":
+            login()
+        elif button_val == "reg"
+            result = check_reg(u, p)
+            if result  == "Username taken":
+                return render_template("login.html",message="Username taken. Be more original")
+            elif result == "Invalid username":
+                return render_template("login.html",message="Invalid username. Please change it.")
+            else:
+                add_account(u, p)
+                return render_template("login.html",message="Success! Your account has been created.")
 
 @app.route("/welcome/")
 def welcome():
@@ -47,9 +51,12 @@ def settings():
     if 'username' in session: #check if user can actually use settings
         return render_template('settings.html') #add more arguments from Lorenz's db util files
 
-@app.route("/contribute", methods=["POST"])
-def add_contribution():
-    
+@app.route("/add_story", methods=["POST"])
+def add_story:
+    text = request.form["storytext"]
+    contributor = request.form["contributor"]
+    title = request.form["title"]
+    dbaccess.add_story(text, contributor, title)
 
 if __name__ == "__main__":
     app.debug = True
--- utils/dbaccess.py
+++ utils/dbaccess.py
@@ -1,8 +1,14 @@
+import sqlite3
+
+
+db = sqlite3.connect(../data/data.db)
+
 def check_reg(username, password):
+
     if not valid_username(username):
         return "Invalid username"
     curs = db.cursor()
-    rows = curs.execute("SELECT name from user where name={input_name}".format(input_name=username))
+    rows = curs.execute("SELECT name from user where name={p_name}".format(p_name=username))
     if rows:
         return "Username taken"
     return "Success"
@@ -13,5 +19,24 @@ def valid_username(username):
 
 def add_account(username, password):
     curs = db.cursor()
-    curs.execute("INSERT INTO users (name, password) VALUES ({input_u}, {input_p})".format(input_u=username, input_p=password))
+    curs.execute("INSERT INTO users (name, password) VALUES ({p_user}, {p_password})".format(p_user=username, p_password=password))
     db.commit()
+
+def add_story(body, title, contributor, uid):
+    # 1. Get the uid of the contributor
+    curs = db.cursor()
+    row = curs.execute("SELECT uid from user where name={p_name}".format(p_name=contributor))
+    story_creator = ""
+    contributors = ""
+    if row:
+        story_creator = row["uid"]
+        contributors = story_creator
+    # 2. Check if story exists.  Story exists if title exists
+    row = curs.execute("SELECT sid from story where title={p_title}".format(p_title=title))
+    if row:
+        return False
+    else:
+        curs.execute("INSERT INTO story (body, title, contributors, uid) VALUES ({p_body}, {p_title}, {p_contributors}, {p_uid})".format(p_body=body, p_title=title, p_contributors=contributors, p_uid=uid))
+        db.commit()
+        return True
+
